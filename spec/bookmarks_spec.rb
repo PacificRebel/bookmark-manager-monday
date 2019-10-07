a229e7a03e3339d3b0845d11094312d2eb663e2f
Nabille Helal <nabilleh@outlook.com>
2019-10-07 16:18:23 +0100

    Add first db setup info

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -5,7 +5,6 @@ describe Bookmark do
   describe '#all' do
     it "should return all bookmarks stored" do
       bookmarks = Bookmark.all
-
       expect(bookmarks).to include("http://www.stackoverflow.com")
       expect(bookmarks).to include("http://www.google.com")
     end
